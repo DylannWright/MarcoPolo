ac33ce43f8af1c0b9b095e3872eda56feaf7bb37
Dylan Wright <17WrighD@fulford.york.sch.uk>
2022-11-14 08:49:00 +0000

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int counter = 0;
+
+            if (counter % 2 == 0 & textBox1.Text == "Marco")
+            {
+                counter += 1;
+            }
+            else if (counter % 2 != 0 & textBox1.Text == "Polo")
+            {
+                counter += 1;
+            }
+        }
+    }
+}
